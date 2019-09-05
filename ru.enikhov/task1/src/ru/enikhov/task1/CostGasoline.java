e94d8688fece60eb662e39aca7b1a38b8d0d7fec
Енихов Владислав <hewlett@mail.ru>
2019-09-06 00:19:08 +0300

    3 commit

---
+++ ru.enikhov/task1/src/ru/enikhov/task1/CostGasoline.java
@@ -0,0 +1,13 @@
+package ru.enikhov.task1;
+
+public class CostGasoline {
+
+    public static void main(String[] args) {
+	    double price;
+	    double volume;
+
+	    price = 43.30;
+	    volume = 25;
+        System.out.println(price*volume);
+    }
+}
---
+++ ru.enikhov/task1/src/ru/enikhov/task2/SecondToHour.java
@@ -0,0 +1,11 @@
+package ru.enikhov.task2;
+
+public class SecondToHour {
+
+    public static void main(String[] args) {
+	    int second;
+	    second = 6000;
+
+        System.out.println(second/(double)3600);
+    }
+}
