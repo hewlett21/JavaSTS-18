577842fdb317f2ce188f55dd01b268173df67bf6
Енихов Владислав <hewlett@mail.ru>
2019-09-10 23:40:54 +0300

    lesson4 First commit

---
+++ lesson4/src/ru/enikhov/task3/MultiplicationTable.java
@@ -0,0 +1,23 @@
+package ru.enikhov.task3;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Укажите до какого числа строить таблицу умножения (от 1 до 10):");
+        int range = scanner.nextInt();
+        if (range > 0 && range < 11) {
+            for (int i = 1; i <= range; i++) {
+                for (int j = 1; j < 11; j++) {
+                    System.out.println(i + "x" + j + " = " + i * j);
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Ваше число " + range + " не попадает в диапазон от 0 до 10.\nПопробуйте еще раз.");
+        }
+        scanner.close();
+    }
+}
