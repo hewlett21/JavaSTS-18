b3de0b3854edccaa6b69522e47cd3f7e510bb8cd
Енихов Владислав <hewlett@mail.ru>
2019-11-08 19:11:44 +0300

    lesson19 Price change commit

--- lessons/src/main/java/ru/enikhov/lesson19/Price.java
+++ lessons/src/main/java/ru/enikhov/lesson19/Price.java
@@ -10,13 +10,17 @@ public class Price {
     public static void main(String[] args) {
         Check check = new Check();
         try (Scanner scanner = new Scanner(new File("products.txt"));) {
-            scanner.useDelimiter("\n");
+//            scanner.useDelimiter("\n");
             scanner.useLocale(Locale.US);   //выбираем локаль, для считывания float
             check.printTitle();             // печатаем шапку чека
             while (scanner.hasNext()) {
                 check.setProduct(scanner.next());       // название продукта
-                check.setQuantity(scanner.nextFloat()); // количество
-                check.setPrice(scanner.nextFloat());    // цена за еденицу товара
+                if (scanner.hasNextFloat()) {
+                    check.setQuantity(scanner.nextFloat()); // количество
+                    check.setPrice(scanner.nextFloat()); // цена за еденицу товара
+                }
+//                check.setQuantity(scanner.nextFloat()); // количество
+//                check.setPrice(scanner.nextFloat());    // цена за еденицу товара
                 check.printCheck();                     // печатаем "тело" чека
             }
             check.printBottom();                        // печатаем итоговую сумму чека
