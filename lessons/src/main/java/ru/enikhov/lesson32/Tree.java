53620bec1eff19e2300b63c62c902d429c8a6373
Енихов Владислав <hewlett@mail.ru>
2019-11-28 01:03:17 +0300

    lesson32 modify commit

---
+++ lessons/src/main/java/ru/enikhov/lesson32/Tree.java
@@ -0,0 +1,95 @@
+package ru.enikhov.lesson32;
+
+class Tree {
+    public class Node {
+        private int iData;
+        private Node leftChild;
+        private Node rightChild;
+
+        private Node(int iData) {
+            this.iData = iData;
+            leftChild = null;
+            rightChild = null;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + iData + "}";
+        }
+
+        private boolean isLeaf() {
+            return leftChild != null | rightChild != null;
+        }
+    }
+
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public Node find(int key) {
+        if (root == null) {
+            return null;        //Дерево пустое;
+        }
+        Node current = root;
+        while (current.iData != key) {
+            if (current.iData > key) {
+                current = current.leftChild;
+            } else {
+                current = current.rightChild;
+            }
+            if (current == null) {
+                return null;
+            }
+        }
+        return current;
+    }
+
+    public void insert(int key) {
+        Node newNode = new Node(key);
+        if (root == null) {
+            root = newNode;
+        } else {
+            Node current = root;
+            Node parent;
+            while (true) {
+                parent = current;
+                if (key < current.iData) {
+                    current = current.leftChild;
+                    if (current == null) {
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                } else {
+                    current = current.rightChild;
+                    if (current == null) {
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void print(Node startNode) {
+        if (startNode != null) {        //условие сработает, когда мы достигним конца дерева и потомков не останется
+            print(startNode.leftChild); //рекурсивно вызываем левых потомков
+            System.out.print(startNode);//печатаем ноду
+            print(startNode.rightChild);//вызов правых
+        }
+    }
+
+
+    public int countLeaf(Node node) {
+        int cnt = 1;
+        if (node == null) {
+            return 0;
+        }
+        if (node.isLeaf()) {
+            cnt = countLeaf(node.leftChild) + countLeaf(node.rightChild);
+        }
+        return cnt;
+    }
+
+}
